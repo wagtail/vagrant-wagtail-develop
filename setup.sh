5b0362d0e847abbb485a5d6720989cb2c286c82d
Matt Westcott <matt@west.co.tt>
2015-06-03 14:55:27 +0100

    remove no-longer-needed vagrant share path patch

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 git clone https://github.com/torchbox/wagtaildemo.git
-# git clone -b fix/vagrant-share-path https://github.com/gasman/wagtaildemo.git
 git clone https://github.com/torchbox/wagtail.git
 vagrant up
