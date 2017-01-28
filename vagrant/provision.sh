874e4d908a4ae9fe537a4c8b4524c9abd3d29a5c
Daniel Chimeno <dchimeno@gmail.com>
2017-01-28 13:35:51 +0100

    add update pip and echoing installing node.js

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -18,8 +18,12 @@ $WAGTAILDEMO_ROOT/vagrant/provision.sh
 # install system-wide developer dependencies
 apt-get install -y libenchant-dev
 
+#Update pip
+su - vagrant -c "$PIP install -U pip"
+
 # install additional dependencies (including developer-specific ones)
 # of wagtail master
+
 su - vagrant -c "cd $WAGTAIL_ROOT && $PIP install -e .[testing,docs] -U"
 
 # install optional packages (so that the full test suite runs)
@@ -39,6 +43,7 @@ su - vagrant -c "$PYTHON $WAGTAILDEMO_ROOT/manage.py migrate --noinput"
 
 # also create a Python 2 environment
 su - vagrant -c "/usr/local/bin/virtualenv $PY2_VIRTUALENV_DIR"
+su - vagrant -c "$PY2_PIP install -U pip" #Update pip
 su - vagrant -c "$PY2_PIP install -r $WAGTAILDEMO_ROOT/requirements.txt"
 su - vagrant -c "cd $WAGTAIL_ROOT && $PY2_PIP install -e .[testing,docs]"
 su - vagrant -c "$PY2_PIP install embedly elasticsearch django-sendfile"
@@ -47,4 +52,5 @@ su - vagrant -c "cd $LIBS_ROOT/Willow && $PYTHON2 setup.py develop"
 
 # Install node.js tooling - do this last, because otherwise the presence of .node_modules
 # makes "pip install -e ." take forever
+echo "Installing node.js tooling..."
 su - vagrant -c "cd $WAGTAIL_ROOT && npm install && npm run build"
